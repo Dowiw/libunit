4596923a78c97c1fef6dae049c4423f2a58ae0f6
monjie12272@gmail.com <monjie12272@gmail.com>
2026-06-14 21:32:48 +0200

    add: floating point comparisons

--- framework/inc/libunit_assert.h
+++ framework/inc/libunit_assert.h
@@ -5,4 +5,10 @@
 # define UNIT_ASSERT_EQ(a, b) if ((a) != (b)) return (-1)
 # define UNIT_ASSERT_NEQ(a, b) if ((a) == (b)) return (-1)
 
+# ifndef UNIT_EPSILON
+#  define UNIT_EPSILON 0.00001f
+# endif
+
+# define UNIT_ASSERT_FEQ(a, b) UNIT_ASSERT(((a) - (b) < UNIT_EPSILON) && ((b) - (a) < UNIT_EPSILON))
+
 #endif
