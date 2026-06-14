6ebabcc580989fb306a4654480d80cdeb923c1e3
monjie12272@gmail.com <monjie12272@gmail.com>
2026-06-15 00:16:36 +0200

    feat: ability to find assert fail

--- framework/inc/libunit_assert.h
+++ framework/inc/libunit_assert.h
@@ -1,14 +1,16 @@
 #ifndef LIBUNIT_ASSERT_H
 # define LIBUNIT_ASSERT_H
 
-# define UNIT_ASSERT(cond) if (!(cond)) return (-1)
-# define UNIT_ASSERT_EQ(a, b) if ((a) != (b)) return (-1)
-# define UNIT_ASSERT_NEQ(a, b) if ((a) == (b)) return (-1)
+void unit_print_error(const char *file, int line, const char *cond);
+
+# define UNIT_ASSERT(cond) if (!(cond)) { unit_print_error(__FILE__, __LINE__, #cond); return (-1); }
+# define UNIT_ASSERT_EQ(a, b) if ((a) != (b)) { unit_print_error(__FILE__, __LINE__, #a " == " #b); return (-1); }
+# define UNIT_ASSERT_NEQ(a, b) if ((a) == (b)) { unit_print_error(__FILE__, __LINE__, #a " != " #b); return (-1); }
 
 # ifndef UNIT_EPSILON
-#  define UNIT_EPSILON 0.00001f
+#  define UNIT_EPSILON 0.00001
 # endif
 
-# define UNIT_ASSERT_FEQ(a, b) UNIT_ASSERT(((a) - (b) < UNIT_EPSILON) && ((b) - (a) < UNIT_EPSILON))
+# define UNIT_ASSERT_FEQ(a, b) if (!(((a) - (b) < UNIT_EPSILON) && ((b) - (a) < UNIT_EPSILON))) { unit_print_error(__FILE__, __LINE__, #a " ~= " #b); return (-1); }
 
 #endif
---
+++ framework/src/libunit_assert.c
@@ -0,0 +1,43 @@
+#include "libunit.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    int i = 0;
+    while (s && s[i])
+        i++;
+    if (i > 0)
+        write(fd, s, i);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+    long ln = n;
+    char c;
+
+    if (ln < 0)
+    {
+        write(fd, "-", 1);
+        ln = -ln;
+    }
+    if (ln > 9)
+    {
+        ft_putnbr_fd(ln / 10, fd);
+        ft_putnbr_fd(ln % 10, fd);
+    }
+    else
+    {
+        c = ln + '0';
+        write(fd, &c, 1);
+    }
+}
+
+void unit_print_error(const char *file, int line, const char *cond)
+{
+    ft_putstr_fd("  [ASSERT FAILED: ", 2);
+    ft_putstr_fd((char *)file, 2);
+    ft_putstr_fd(":", 2);
+    ft_putnbr_fd(line, 2);
+    ft_putstr_fd(" (", 2);
+    ft_putstr_fd((char *)cond, 2);
+    ft_putstr_fd(")] ", 2);
+}
